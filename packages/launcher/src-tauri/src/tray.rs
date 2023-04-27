8bc9bfe2ef98f72e464add7321e8172115b1134d
meowtec <bertonzh@gmail.com>
2023-04-27 23:14:33 +0800

    feat(app): use tray_icon only on macOS

--- packages/launcher/src-tauri/src/tray.rs
+++ packages/launcher/src-tauri/src/tray.rs
@@ -1,6 +1,4 @@
-use tauri::{SystemTray, SystemTrayEvent};
-
-use tauri::{CustomMenuItem, SystemTrayMenu};
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu};
 
 use crate::window::reopen_window;
 
@@ -9,21 +7,27 @@ pub fn init_tray(app: &tauri::App) {
     let tray_menu =
         SystemTrayMenu::new().add_item(CustomMenuItem::new("quit".to_string(), "Quit".to_string()));
 
-    SystemTray::new()
-        .with_menu(tray_menu)
-        .on_event(move |event| match event {
-            #[allow(clippy::single_match)]
-            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                "quit" => {
-                    handle.exit(0);
-                }
-                _ => {}
-            },
-            SystemTrayEvent::LeftClick { .. } => {
-                reopen_window(&handle);
+    #[allow(unused_mut)]
+    let mut tray = SystemTray::new().with_menu(tray_menu);
+
+    #[cfg(target_os = "macos")]
+    {
+        tray = tray.with_icon(Icon::Raw(include_bytes!("../icons/icon_tray.png").to_vec()));
+    }
+
+    tray.on_event(move |event| match event {
+        #[allow(clippy::single_match)]
+        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+            "quit" => {
+                handle.exit(0);
             }
             _ => {}
-        })
-        .build(app)
-        .unwrap();
+        },
+        SystemTrayEvent::LeftClick { .. } => {
+            reopen_window(&handle);
+        }
+        _ => {}
+    })
+    .build(app)
+    .unwrap();
 }
